658685f7fa0eb4724bdcf8f1bd5b37429b0058f3
Omar Nasser <omarnasserjr@gmail.com>
2026-06-16 18:57:04 +0300

    feat: Add ValidateLogger simple logic

--- go.mod
+++ go.mod
@@ -2,7 +2,14 @@ module github.com/onasser1/validating-kontroller
 
 go 1.26.2
 
-require k8s.io/apiserver v0.36.1
+require (
+	github.com/spf13/pflag v1.0.9
+	k8s.io/api v0.36.1
+	k8s.io/apimachinery v0.36.1
+	k8s.io/apiserver v0.36.1
+	k8s.io/component-base v0.36.1
+	k8s.io/klog/v2 v2.140.0
+)
 
 require (
 	cel.dev/expr v0.25.1 // indirect
@@ -40,13 +47,13 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.3-0.20250322232337-35a7c28c31ee // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/onasser1/kontroller v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/prometheus/client_golang v1.23.2 // indirect
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.67.5 // indirect
 	github.com/prometheus/procfs v0.19.2 // indirect
 	github.com/spf13/cobra v1.10.2 // indirect
-	github.com/spf13/pflag v1.0.9 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.etcd.io/etcd/api/v3 v3.6.8 // indirect
@@ -83,11 +90,7 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.36.1 // indirect
-	k8s.io/apimachinery v0.36.1 // indirect
 	k8s.io/client-go v0.36.1 // indirect
-	k8s.io/component-base v0.36.1 // indirect
-	k8s.io/klog/v2 v2.140.0 // indirect
 	k8s.io/kms v0.36.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20260317180543-43fb72c5454a // indirect
 	k8s.io/streaming v0.36.1 // indirect
--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"time"
 
+	logger "github.com/onasser1/kontroller/pkg/apis/okofs/v1alpha1"
 	"github.com/onasser1/validating-kontroller/pkg/options"
 	"github.com/spf13/pflag"
+	admissionv1beta1 "k8s.io/api/admission/v1beta1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apiserver/pkg/server"
 	"k8s.io/component-base/cli/globalflag"
 	"k8s.io/klog/v2"
+)
+
+var (
+	scheme = runtime.NewScheme()
+	codecs = serializer.NewCodecFactory(scheme)
 )
 
 func main() {
@@ -42,4 +53,46 @@ func main() {
 
 func ServeValidationAdmission(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hi, we are ServeValidationAdmission function, we are called")
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		klog.Fatalf("error reading body from request: %v", err)
+	}
+	// Get GVK of AdmissionReview
+	admissionReviewGVK := admissionv1beta1.SchemeGroupVersion.WithKind("AdmissionReview")
+	AdmissionReview := admissionv1beta1.AdmissionReview{}
+	_, _, err = codecs.UniversalDeserializer().Decode(body, &admissionReviewGVK, &AdmissionReview)
+	if err != nil {
+		klog.Fatalf("error deserializing the AdmissionReview object: %v", err)
+	}
+
+	// Get GVK of Logger type
+	loggerGVK := logger.SchemaGroupVersion.WithKind("Logger")
+	Logger := logger.Logger{}
+	_, _, err = codecs.UniversalDeserializer().Decode(AdmissionReview.Request.Object.Raw, &loggerGVK, &Logger)
+	if err != nil {
+		klog.Fatalf("error deserializing the Logger object: %v", err)
+	}
+	klog.Infof("Logger Resource we got is: %v", Logger)
+
+	AdmissionResponse := admissionv1beta1.AdmissionResponse{}
+	if allowed := ValidateLogger(); !allowed {
+		AdmissionResponse = admissionv1beta1.AdmissionResponse{
+			UID:     AdmissionReview.Request.UID,
+			Allowed: allowed,
+			Result: &v1.Status{
+				Message: "Not Allowed, currently we are rejecting all Logger requests through the webhook",
+			},
+		}
+	} else {
+		AdmissionResponse = admissionv1beta1.AdmissionResponse{
+			UID:     AdmissionReview.Request.UID,
+			Allowed: allowed,
+		}
+	}
+	klog.Infof("AdmissionResponse that is being sent: %v", AdmissionResponse)
+}
+
+// ValidateLogger currently rejects all requests for Logger
+func ValidateLogger() bool {
+	return false
 }
